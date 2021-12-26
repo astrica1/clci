70dce29baca05e1ebdd29eac47c0f9d8911c721b
a.matani <a.matani@nizek.com>
2021-12-26 21:17:29 +0330

    add example for playfair cipher algorithm

---
+++ examples/example_playfair.py
@@ -0,0 +1,39 @@
+import os
+from clci import playfair
+
+def ClearConsole():
+    command = 'clear'
+    if os.name in ('nt', 'dos'):
+        command = 'cls'
+    os.system(command)
+
+def Encryption():
+    planeText = input('Plane text: ').upper().replace(' ', '')
+    key = int(input('Key number: ')).upper().replace(' ', '')
+    print(playfair.Encrypt(planeText, key))
+
+def Decryption():
+    cipher = input('Cipher text: ').upper().replace(' ', '')
+    key = int(input('Key number: ')).upper().replace(' ', '')
+    print(playfair.Decrypt(cipher, key))
+    
+    
+def main():
+    while True:
+        print('Enter \'e\' for Encryption')
+        print('Enter \'d\' for Decryption')
+        char = input('Application mode: ').lower()
+        ClearConsole()
+        if char == 'd':
+            print('Decryption')
+            print('==========\n')
+            Decryption()
+            exit(0)
+        elif char == 'e':
+            print('Encryption')
+            print('==========\n')
+            Encryption()
+            exit(0)
+
+if __name__ == "__main__":
+    main()
